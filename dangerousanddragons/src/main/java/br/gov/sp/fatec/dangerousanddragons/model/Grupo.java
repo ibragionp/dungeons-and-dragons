5fa00687ecfbc671652fd4aaf3a462bb43422faa
ibragionp <isabellabpereira@protonmail.com>
2021-04-14 18:04:55 -0300

    Adição de extensões de personagens Feiticeiro e Lutador, bem como grupo para testes futuros

---
+++ dangerousanddragons/src/main/java/br/gov/sp/fatec/dangerousanddragons/model/Grupo.java
@@ -0,0 +1,55 @@
+package br.gov.sp.fatec.dangerousanddragons.model;
+
+import java.util.Set;
+import javax.persistence.Table;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.ManyToOne;
+import javax.persistence.JoinTable;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToMany;
+import javax.persistence.AttributeOverride;
+
+
+@Entity
+@Table(name = "GRU_GRUPO")
+@AttributeOverride(name = "id", column = @Column(name = "GRU_ID"))
+public class Grupo extends GeraId {
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "GRU_MESTRE")
+    private Usuario mestre;
+
+    @Column(name = "GRU_NOME")
+    private String nomeGrupo;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "INT_INTEGRANTE",
+            joinColumns = { @JoinColumn(name = "GRU_ID")},
+            inverseJoinColumns = { @JoinColumn(name = "PER_ID")}
+    )
+    private Set<Personagem> integrantes;
+
+
+    public Usuario getMestre() {
+        return mestre;
+    }
+    public void setMestre(Usuario mestre) {
+        this.mestre = mestre;
+    }
+
+    public Set<Personagem> getIntegrantes() {
+        return integrantes;
+    }
+    public void setIntegrantes(Set<Personagem> integrantes) {
+        this.integrantes = integrantes;
+    }
+
+    public String getNomeGrupo() {
+        return nomeGrupo;
+    }
+    public void setNomeGrupo(String nomeGrupo) {
+        this.nomeGrupo = nomeGrupo;
+    }
+}
