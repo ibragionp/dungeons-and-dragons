ffafdeb1cb05793f438a1131906413cfc1f640e0
Lucas José Povinske <lucasjp96@gmail.com>
2021-04-27 13:46:02 -0300

    Update on paths

--- front_end_view/src/router/index.js
+++ front_end_view/src/router/index.js
@@ -3,7 +3,7 @@ import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 import Login from '../views/Login.vue'
 import Usuario from '../views/Usuario.vue'
-import store from '../store'
+import store from '../store' 
 
 Vue.use(VueRouter)
 
