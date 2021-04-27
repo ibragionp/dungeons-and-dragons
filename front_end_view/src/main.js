35b940cce6028778a75adfaae83b72397c8b2ed3
Lucas José Povinske <lucasjp96@gmail.com>
2021-04-27 13:22:13 -0300

    Update on npm

--- front_end_view/src/main.js
+++ front_end_view/src/main.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
-import App from './App.vue'
-import store from './store'
-import router from './router'
+import App from './front_end_view/App.vue'
+import store from './front_end_view/store'
+import router from './front_end_view/router'
 import axios from 'axios'
 
 axios.defaults.baseURL = 'https://8080-a99bf943-ee05-4766-878b-66f9e91d7689.ws-us02.gitpod.io'
