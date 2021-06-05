6060280dae05b1d43731123f704de143e3dc8a9b
Railson Tales <railson.tales@outlook.com>
2021-06-05 19:50:38 -0300

    testaInsercaoFeiticeiro

--- springboot-dungeonsanddragons/src/test/java/com/fatec/springbootdungeonsanddragons/SpringBootAppApplicationTest.java
+++ springboot-dungeonsanddragons/src/test/java/com/fatec/springbootdungeonsanddragons/SpringBootAppApplicationTest.java
@@ -7,6 +7,8 @@ import java.util.List;
 
 import org.junit.jupiter.api.Test;
 
+import com.fatec.springbootdungeonsanddragons.entity.Feiticeiro;
+import com.fatec.springbootdungeonsanddragons.entity.Personagem;
 import com.fatec.springbootdungeonsanddragons.entity.Usuario;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -14,6 +16,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.fatec.springbootdungeonsanddragons.repository.FeiticeiroRepository;
+import com.fatec.springbootdungeonsanddragons.repository.LutadorRepository;
 import com.fatec.springbootdungeonsanddragons.repository.UsuarioRepository;
 import com.fatec.springbootdungeonsanddragons.service.UsuarioService;
 
@@ -26,15 +29,17 @@ class SpringBootAppApplicationTests {
     private UsuarioRepository usuarioRepo;
     
     @Autowired
-    private FeiticeiroRepository feitiRepo;
+    private FeiticeiroRepository feiticeiroRepo;
 
     @Autowired
-    private UsuarioService usuarioService;
-
+    private LutadorRepository lutadorRepo;
+    
     // TESTES PRINCIPAIS
 	@Test
 	void contextLoads() {
     }
+
+    // TESTES DE INSERIR USUARIO
     @Test
     void testaInsercao() {
         Usuario usuario = new Usuario();
@@ -46,7 +51,7 @@ class SpringBootAppApplicationTests {
         assertNotNull(usuario.getId());
     }
 
-    // TESTES DE BUSCA
+    // TESTES DE BUSCA USUARIO
     @Test
     void testaBuscaUsuarioNomeContains() {
         Usuario usuario = new Usuario();
@@ -59,4 +64,29 @@ class SpringBootAppApplicationTests {
         System.out.print(usuarios);
         assertTrue(!usuarios.isEmpty());
     }
+
+    // Testes de inserir Feiticeiro
+    @Test
+    void testaInsercaoFeiticeiro() {
+        Feiticeiro feiticeiro = new Feiticeiro();
+        
+        // feiticeiro.setProprietario(usuario);
+        feiticeiro.setNome("Shang Tsung");
+        feiticeiro.setRaca("Chinês");
+        feiticeiro.setClasse("Feiticeiro");
+        feiticeiro.setNivel(1);
+        feiticeiro.setAntecedente("John Tobias");
+        feiticeiro.setForca(2);
+        feiticeiro.setDestreza(3);
+        feiticeiro.setConstituicao(4);
+        feiticeiro.setInteligencia(5);
+        feiticeiro.setSabedoria(6);
+        feiticeiro.setCarisma(7);
+        feiticeiro.setMagiasConhecidas(8);
+        feiticeiro.setModificadorHabilidade(9);
+
+        feiticeiroRepo.save(feiticeiro);
+        assertNotNull(feiticeiro.getId());
+    }
+    
 }
