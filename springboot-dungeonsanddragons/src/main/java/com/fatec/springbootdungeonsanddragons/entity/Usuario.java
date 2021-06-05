0890be632684dd8057c4d167477f34a67730274b
Vitor Henrique da Silva <vhsilva@fitec.org.br>
2021-06-05 17:28:42 -0300

    Trocando AUTO por IDENTITY

--- springboot-dungeonsanddragons/src/main/java/com/fatec/springbootdungeonsanddragons/entity/Usuario.java
+++ springboot-dungeonsanddragons/src/main/java/com/fatec/springbootdungeonsanddragons/entity/Usuario.java
@@ -10,7 +10,7 @@ import java.util.UUID;
 public class Usuario {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "USU_ID")
     private Long id;
 
